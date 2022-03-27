16f09ce02b8120301cd3a2bdb7617a17b7660317
Макс Кузнецов <orenburg20172017@gmail.com>
2022-03-28 01:14:49 +0300

    Приложение для расчета индекса массы тела.

--- BmiService/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService index = new BmiService();
-        int result = index.calculate(90, 2);
-        System.out.print("Ваш индекс массы тела=" + result);
-    }
-}
