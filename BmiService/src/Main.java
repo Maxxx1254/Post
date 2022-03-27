fbf9ac07b2ecad187d92da0e6cafbb8fcc646868
Макс Кузнецов <orenburg20172017@gmail.com>
2022-03-28 01:19:42 +0300

    Revert "Приложение для расчета индекса массы тела."
    This reverts commit 16f09ce02b8120301cd3a2bdb7617a17b7660317.

---
+++ BmiService/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService index = new BmiService();
+        int result = index.calculate(90, 2);
+        System.out.print("Ваш индекс массы тела=" + result);
+    }
+}
